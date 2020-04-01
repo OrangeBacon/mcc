c9080b6dc2ce8cd0b1d69ae10bc505bb9d2eae11
OrangeBacon <computer.backup.15@gmail.com>
2020-04-01 17:56:56 +0100

    Updated symbol table - now global variables can be defined/used - uses hash table for them, rather than iterating an array - added so functions can be implemented properly

--- src/symbolTable.c
+++ src/symbolTable.c
@@ -1,5 +1,7 @@
 #include "symbolTable.h"
 
+#include <string.h>
+
 static uint32_t stringHash(const char* str, unsigned int length) {
     uint32_t hash = 2166126261u;
 
@@ -13,6 +15,7 @@ static uint32_t stringHash(const char* str, unsigned int length) {
 
 void SymbolTableInit(SymbolTable* table) {
     ARRAY_ALLOC(SymbolLocal*, *table, local);
+    TABLE_INIT(table->globals, SymbolGlobal);
 }
 
 
@@ -49,6 +52,31 @@ SymbolLocal* SymbolTableGetLocal(SymbolTable* table, const char* name, unsigned 
     return NULL;
 }
 
+bool SymbolTableIsGlobal(SymbolTable* table, const char* name, unsigned int length) {
+    SymbolGlobal* global = tableGet(&table->globals, name, length);
+    return global != NULL;
+}
+
+SymbolGlobal* SymbolTableAddGlobal(SymbolTable* table, const char* name, unsigned int length) {
+    SymbolGlobal* global = SymbolTableGetGlobal(table, name, length);
+    if(global != NULL) {
+        return NULL;
+    }
+
+    SymbolGlobal* ret = ArenaAlloc(sizeof(*global));
+    ret->name = name;
+    ret->length = length;
+    ret->hash = stringHash(name, length);
+
+    TABLE_SET(table->globals, name, length, ret);
+
+    return ret;
+}
+
+SymbolGlobal* SymbolTableGetGlobal(SymbolTable* table, const char* name, unsigned int length) {
+    return tableGet(&table->globals, name, length);
+}
+
 void SymbolTableEnter(SymbolTable* table) {
     table->currentDepth++;
 }
@@ -65,4 +93,135 @@ SymbolExitList* SymbolTableExit(SymbolTable* table) {
     }
 
     return ret;
+}
+
+// find an entry in a list of entries
+static Entry* findEntry(Entry* entries, int capacity, Key* key) {
+
+    // location to start searching
+    uint32_t index = key->hash % capacity;
+
+    while(true) {
+        // value to check
+        Entry* entry = &entries[index];
+
+        // if run out of entries to check or the correct entry is found,
+        // return current search item
+        if(entry->key.key == NULL || (entry->key.length == key->length && strcmp(entry->key.key, key->key) == 0)) {
+            return entry;
+        }
+
+        // get next location, wrapping round to index 0
+        index = (index + 1) % capacity;
+    }
+}
+
+// increase the size of a table to the given capacity
+void adjustCapacity(Table* table, unsigned int capacity) {
+
+    // create new data section and null initialise
+    Entry* entries = ArenaAlloc(sizeof(Entry) * capacity);
+    for(unsigned int i = 0; i < capacity; i++) {
+        entries[i].key.key = NULL;
+        entries[i].value = NULL;
+    }
+
+    // copy data from the table to the new data section
+    // re-organises table for new memory size not just
+    // copying the data across.
+    if(table->entryCount > 0) {
+        for(unsigned int i = 0; i < table->entryCapacity; i++) {
+            Entry* entry = &table->entrys[i];
+            if(entry->key.key == NULL) {
+                continue;
+            }
+
+            Entry* dest = findEntry(entries, capacity, &entry->key);
+            dest->key = entry->key;
+            dest->value = entry->value;
+        }
+    }
+
+    // set the table's data to the new data
+    table->entrys = entries;
+    table->entryCapacity = capacity;
+}
+
+void tableSet(Table* table, const char* keyPtr, unsigned int length, void* valuePtr) {
+    // create the key to be inserted into the table
+    Key key;
+    key.key = keyPtr;
+    key.length = length;
+    key.hash = stringHash(keyPtr, length);
+
+    // make sure the table is big enough
+    if(table->entryCount + 1 > table->entryCapacity * TABLE_MAX_LOAD) {
+        int capacity = table->entryCapacity;
+        capacity = capacity < 8 ? 8 : capacity * 2;
+        adjustCapacity(table, capacity);
+    }
+
+    // get the entry to be set
+    Entry* entry = findEntry(table->entrys, table->entryCapacity, &key);
+
+    // does the entry have any content?
+    bool isNewKey = entry->key.key == NULL;
+    if(isNewKey) {
+        table->entryCount++;
+    }
+
+    entry->key = key;
+    entry->value = valuePtr;
+}
+
+void* tableGet(Table* table, const char* keyPtr, unsigned int length) {
+    // if nothing has been set then get will always be false
+    if(table->entrys == NULL) {
+        return false;
+    }
+
+    // create key to search for
+    Key key;
+    key.key = keyPtr;
+    key.length = length;
+    key.hash = stringHash(keyPtr, length);
+
+    // find location where the key should be
+    Entry* entry = findEntry(table->entrys, table->entryCapacity, &key);
+    if(entry->key.key == NULL) {
+        return NULL;
+    }
+
+    return entry->value;
+}
+
+bool tableHas(Table* table, const char* key, unsigned int length) {
+    if(table->entrys == NULL) {
+        return false;
+    }
+
+    Key test;
+    test.key = key;
+    test.length = length;
+    test.hash = stringHash(key, length);
+
+    Entry* entry = findEntry(table->entrys, table->entryCapacity, &test);
+
+    // if the item is in the table, its key will not be null
+    return !(entry->key.key == NULL);
+}
+
+void tableRemove(Table* table, const char* key, unsigned int length) {
+    if(table->entrys == NULL) {
+        return;
+    }
+
+    Key test;
+    test.key = key;
+    test.length = length;
+    test.hash = stringHash(key, length);
+
+    Entry* entry = findEntry(table->entrys, table->entryCapacity, &test);
+    entry->key.key = NULL;
+    entry->value = NULL;
 }
--- src/symbolTable.h
+++ src/symbolTable.h
@@ -1,8 +1,37 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
+#define __USE_MINGW_ANSI_STDIO 1
+#include <stdio.h>
 #include <inttypes.h>
 #include "memory.h"
+
+// Hash table implementation
+
+typedef struct Key {
+    const char* key;
+    unsigned int length;
+    uint32_t hash;
+} Key;
+
+typedef struct Entry {
+    Key key;
+
+    void* value;
+} Entry;
+
+typedef struct Table {
+    // hash table
+    ARRAY_DEFINE(Entry, entry);
+
+    size_t valueSize;
+} Table;
+
+void adjustCapacity(Table* table, unsigned int capacity);
+void tableSet(Table* table, const char* key, unsigned int length, void* value);
+void* tableGet(Table* table, const char* key, unsigned int length);
+bool tableHas(Table* table, const char* key, unsigned int length);
+void tableRemove(Table* table, const char* key, unsigned int length);
 
 typedef struct SymbolLocal {
     const char* name;
@@ -14,8 +43,15 @@ typedef struct SymbolLocal {
     int stackOffset;
 } SymbolLocal;
 
+typedef struct SymbolGlobal {
+    const char* name;
+    unsigned int length;
+    uint32_t hash;
+} SymbolGlobal;
+
 typedef struct SymbolTable {
     ARRAY_DEFINE(SymbolLocal*, local);
+    Table globals;
     unsigned int currentDepth;
 } SymbolTable;
 
@@ -29,8 +65,35 @@ SymbolLocal* SymbolTableAddLocal(SymbolTable* table, const char* name, unsigned 
 
 SymbolLocal* SymbolTableGetLocal(SymbolTable* table, const char* name, unsigned int length);
 
+bool SymbolTableIsGlobal(SymbolTable* table, const char* name, unsigned int length);
+
+SymbolGlobal* SymbolTableAddGlobal(SymbolTable* table, const char* name, unsigned int length);
+
+SymbolGlobal* SymbolTableGetGlobal(SymbolTable* table, const char* name, unsigned int length);
+
 void SymbolTableEnter(SymbolTable* table);
 
 SymbolExitList* SymbolTableExit(SymbolTable* table);
+
+
+// hash table convinience macros
+
+#define TABLE_MAX_LOAD 0.75
+
+#define TABLE_INIT(table, vType) \
+    do { \
+        table.valueSize = sizeof(vType); \
+        ARRAY_ZERO(table, entry); \
+        adjustCapacity(&(table), 8); \
+    } while(0)
+
+#define TABLE_SET(table, key, length, value) \
+    do { \
+        if(sizeof(*value) != (table).valueSize) { \
+            printf("Seting table member based on incorrect value size, correct " \
+                "is %zu, got %zu", (table).valueSize, sizeof(value)); \
+        } \
+        tableSet(&(table), (char*)(key), (length), (void*)(value)); \
+    } while(0)
 
 #endif
